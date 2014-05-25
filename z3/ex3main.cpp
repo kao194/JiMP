89a345ff985e66fa9b4021e0fd2b6833ab661db1
kao194 <krzysztof.waclawik.kao@gmail.com>
2014-05-25 15:36:12 +0200

    modifying something - again + testing git

--- z3/ex3main.cpp
+++ z3/ex3main.cpp
@@ -6,7 +6,10 @@ int main()
    generator* a = new gen1(0, 10, 20);
    generator* b = new gen2(0, 10, 20);
    generator* c = new gen3(0, 10, 20);
-   cout << a->randomize() << endl << b->randomize() << endl << c->randomize() << endl;
+   for(int i=0;i<10;i++)
+   {
+      cout << a->randomize() << endl << b->randomize() << endl << c->randomize() << endl;
+   }
    cout << a->getMax();
 	return 0;
 }
